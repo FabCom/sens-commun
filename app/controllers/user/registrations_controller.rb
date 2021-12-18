370fecac194848c00c49d7d54f9d4847290adad8
Louis-Lorgere <louis.lorgere@gmail.com>
2021-12-18 17:56:21 +0100

    modif to stakeholder and registration

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,10 +1,20 @@
 class User::RegistrationsController < Devise::RegistrationsController
-
+  after_action :create_profile, only: [:create]
   # POST /resource
   def create 
-    super do |resource|
+    super
+  end
+
+  private
+
+  def create_profile
+    if resource.id
+      puts "*" * 50
+      p resource
+
+      puts "*" * 50
       Profile.create(user: resource, city: City.first )
-    end 
+    end
   end
 
 end
